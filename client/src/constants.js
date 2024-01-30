da7844e766e589a237f05002807444ade61a8ceb
tharunm1 <maddelatharun2@gmail.com>
2024-01-30 13:26:44 +0530

    frontend deployed link

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,2 +1,2 @@
 export const backendBaseUrl = "https://quizzie-server-k2y6.onrender.com";
-export const frontEndBaseUrl = "http://localhost:3000";
+export const frontEndBaseUrl = "https://celebrated-cendol-0d257c.netlify.app";
