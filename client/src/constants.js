672623ab9479f7681c39c7288c7c23546b26a730
tharunm1 <maddelatharun2@gmail.com>
2024-01-30 19:29:08 +0530

    updated constants.js

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,3 +1,3 @@
-export const backendBaseUrl = "http://quizzie-server-k2y6.onrender.com";
+export const backendBaseUrl = "https://quizzie-server-k2y6.onrender.com";
 
 export const frontEndBaseUrl = "http://localhost:3000";
